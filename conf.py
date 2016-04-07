6cd063a6e6a45f97ebd77a0aaf6242af283ed2d9
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-04-07 16:13:54 +0800

    Reorganize blog page

--- conf.py
+++ conf.py
@@ -154,7 +154,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    '**': ['localtoc.html','sourcelink.html','todo.html','blog.html','searchbox.html']
+    '**': ['localtoc.html','sourcelink.html','todo.html','searchbox.html']
 }
 
 # Additional templates that should be rendered to pages, maps page names to
